72bbb2b6d8a98f2ceaa71150c4f0b5cec9040f97
shivakumarglti <85168862+shivakumarglti@users.noreply.github.com>
2021-06-14 19:39:49 +0530

    Update domain_join_automation_centos.sh

--- domain_join_automation_centos.sh
+++ domain_join_automation_centos.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 read -p "username: " user_name
 read -sp "password: " password
-sudo hostnamectl set-hostname centos7new1
-sudo sed -i 's/.*10.0.0.95.*/10.0.0.95 centos7new1.personallab.local centos7new1/' /etc/hosts
-sudo sed -i '/^search.*/a nameserver 10.0.0.15' /etc/resolv.conf
-sudo sed -i 's/search/search personallab.local/g' /etc/resolv.conf
+sudo hostnamectl set-hostname shivsbxltst9001
+sudo -- sh -c "echo '10.244.5.33 shivsbxltst9001.ENDURANCEBERMUDA.NET shivsbxltst9001' >> /etc/hosts"
+sudo sed -i '/^search.*/a nameserver 10.125.3.60' /etc/resolv.conf
+sudo sed -i '/^search.*/a nameserver 10.122.5.30' /etc/resolv.conf
+sudo sed -i 's/search/search ENDURANCEBERMUDA.NET/g' /etc/resolv.conf
 sudo yum install krb5-workstation -y
 sudo yum install chrony -y
 sudo yum install adcli -y
@@ -13,20 +14,23 @@ sudo yum install samba -y
 sudo yum install sssd-tools -y
 sudo yum install sssd -y
 sudo yum install realmd -y
-sudo -- sh -c "echo 'server personallab.local' >> /etc/chrony.conf"
+sudo yum install oddjob -y
+sudo yum install oddjob-mkhomedir -y
+sudo yum install openldap-clients -y
+sudo -- sh -c "echo 'server ENDURANCEBERMUDA.NET' >> /etc/chrony.conf"
 sudo systemctl restart chronyd.service
 sudo timedatectl
 sudo chronyc sources
-echo $password | kinit -V $user_name@PERSONALLAB.LOCAL
-echo $password | sudo realm join --verbose --user=$user_name PERSONALLAB.LOCAL
+echo $password | kinit -V $user_name@ENDURANCEBERMUDA.NET
+echo $password | sudo realm join --verbose ENDURANCEBERMUDA.NET
 sudo sed -i 's/use_fully_qualified_names = True/use_fully_qualified_names = False/g' /etc/sssd/sssd.conf
 sudo sed -i 's/.*fallback_homedir.*/fallback_homedir = \/home\/%u/g' /etc/sssd/sssd.conf
 sudo sh -c "echo 'entry_cache_timeout = 900' >> /etc/sssd/sssd.conf"
 sudo systemctl start sssd.service
-sudo realm permit -g AccAdminSecOpsServers@PERSONALLAB.LOCAL
-sudo realm permit -g domain\ admins@PERSONALLAB.LOCAL
+sudo realm permit -g AccAdminSecOpsServers@ENDURANCEBERMUDA.NET
+sudo realm permit -g domain\ admins@ENDURANCEBERMUDA.NET
 sudo sed -i 's/.*PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sudo systemctl restart sshd.service
 sudo systemctl restart sssd.service
-sudo sh -c "echo '%AccAdminSecOpsServers@PERSONALLAB.LOCAL ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers"
-sudo sh -c "echo '%domain\ admins@PERSONALLAB.LOCAL ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers"
+sudo sh -c "echo '%AccAdminSecOpsServers@ENDURANCEBERMUDA.NET ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers"
+sudo sh -c "echo '%domain\ admins@ENDURANCEBERMUDA.NET ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers"
